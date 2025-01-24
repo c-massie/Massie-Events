c6a9bef4cf9aec38b715c92437547972b8026975
c-massie <77603178+c-massie@users.noreply.github.com>
2025-01-24 19:53:12 +0000

    Added inline documentation to ProtectedEvent.
    Added inline documentation to ProtectedEvent.

--- MassieEvents/ProtectedEvent.cs
+++ MassieEvents/ProtectedEvent.cs
@@ -2,11 +2,27 @@ using Scot.Massie.Events.Args;
 
 namespace Scot.Massie.Events;
 
+/// <summary>
+/// Wrapper for events allowing you to register listeners and dependent events to the wrapped event, while explicitly
+/// not allowing you to invoke it or examine the listeners.
+/// </summary>
+/// <remarks>
+/// This is intended to wrap events such that they can be made available on the public interface in a way that doesn't
+/// allow invocation, and prevents access to invocation method via casting.
+/// </remarks>
+/// <typeparam name="TArgs">
+/// The type of the event args. This will be a type that encloses information relating to the event. e.g. an event for
+/// a sound playing might include fields in the event args object relating to which sound, or the volume.
+/// </typeparam>
 public class ProtectedEvent<TArgs> : IEvent<TArgs> 
     where TArgs : IEventArgs
 {
     private readonly IEvent<TArgs> _inner;
 
+    /// <summary>
+    /// Wraps the given event in a new protected event instance.
+    /// </summary>
+    /// <param name="inner">The event to wrap.</param>
     public ProtectedEvent(IEvent<TArgs> inner)
     {
         _inner = inner;
