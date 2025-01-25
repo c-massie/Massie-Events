8f561b2ac220b222c5b6082cf1bc7ef1c4f93250
c-massie <77603178+c-massie@users.noreply.github.com>
2025-01-25 23:54:15 +0000

    Added inline documentation to ProtectedPriorityEvent.
    Added inline documentation to ProtectedPriorityEvent.

--- MassieEvents/ProtectedPriorityEvent.cs
+++ MassieEvents/ProtectedPriorityEvent.cs
@@ -2,11 +2,27 @@ using Scot.Massie.Events.Args;
 
 namespace Scot.Massie.Events;
 
+/// <summary>
+/// Wrapper for events allowing you to register listeners (with priority) and dependent events to the wrapped event,
+/// while explicitly not allowing you to invoke it or examine the listeners.
+/// </summary>
+/// <remarks>
+/// This is intended to wrap events such that they can be made available on the public interface in a way that doesn't
+/// allow invocation, and prevents access to invocation method via casting.
+/// </remarks>
+/// <typeparam name="TArgs">
+/// The type of the event args. This will be a type that encloses information relating to the event. e.g. an event for
+/// a sound playing might include fields in the event args object relating to which sound, or the volume.
+/// </typeparam>
 public class ProtectedPriorityEvent<TArgs> : IPriorityEvent<TArgs>
     where TArgs : IEventArgs
 {
     private readonly IPriorityEvent<TArgs> _inner;
 
+    /// <summary>
+    /// Wraps the given event in a new protected event instance with support for registering listeners with priority.
+    /// </summary>
+    /// <param name="inner">The event to wrap.</param>
     public ProtectedPriorityEvent(IPriorityEvent<TArgs> inner)
     {
         _inner = inner;
