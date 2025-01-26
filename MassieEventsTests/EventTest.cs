2d0a079b2b08555e9cbf86b051b0a90ec8a203b6
c-massie <77603178+c-massie@users.noreply.github.com>
2025-01-26 00:00:56 +0000

    Added tests for Event.
    Added tests for Event.

--- MassieEventsTests/EventTest.cs
+++ MassieEventsTests/EventTest.cs
@@ -1,17 +1,26 @@
+using MassieEventsTests.Dummies;
+using Scot.Massie.Events;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace MassieEventsTests;
 
-public class EventTest
+public class EventTest : IInvocableEventTest
 {
-    public ITestOutputHelper Output;
-
     public EventTest(ITestOutputHelper output)
+        : base(output)
     {
-        Output = output;
+        
     }
-    
-    // TO DO: Add tests by inheriting from IInvocableEventTest.
+
+    public override IInvocableEvent<EventArgsWithString> MakeEvent()
+    {
+        return new Event<EventArgsWithString>();
+    }
+
+    public override IInvocableEvent<EventArgsWithInt> MakeDifferentEvent()
+    {
+        return new Event<EventArgsWithInt>();
+    }
 }
 
