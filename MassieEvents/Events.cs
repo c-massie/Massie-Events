8d80fb8128daf70cf1c35bbfed5c1c09307b6191
c-massie <77603178+c-massie@users.noreply.github.com>
2025-02-01 01:13:54 +0000

    Added overload for Events.InvokeMultiple
    Added overloads for Events.InvokeMultiple that pass the same eventargs object to an IEnumerable of events.

--- MassieEvents/Events.cs
+++ MassieEvents/Events.cs
@@ -51,5 +51,62 @@ public static class Events
         InvokeMultiple((IEnumerable<(IInvocableEvent, IEventArgs)>)toInvoke);
     }
 
+    /// <summary>
+    /// Invokes multiple events together at the same time, passed the same IEventArgs object to each one, which may be
+    /// used to generate the IEventArgs objects passed to any dependent events.
+    ///
+    /// Listeners from all events are called in a single pool respecting priority if applicable. e.g. if event A and
+    /// event B are invoked together with this static method, and event A has a listener with a higher priority than
+    /// event B's listener, and a listener with a lower priority than event B's listener, then event A's lower priority
+    /// listener will be called first, followed by event B's listener, followed by event A's higher priority listener.
+    /// </summary>
+    /// <param name="toInvoke">The events to invoke.</param>
+    /// <param name="args">The event args to pass to all directly invoked events.</param>
+    public static void InvokeMultiple(IEnumerable<IInvocableEvent> toInvoke, IEventArgs args)
+    {
+        var callInfo             = Enumerable.Empty<IEventListenerCallInfo>();
+        var listenerOrderMatters = false;
+
+        foreach(var ev in toInvoke)
+        {
+            callInfo             = callInfo.Concat(ev.GenerateCallInfo(args, out var listenerOrderMatterForThisEvent));
+            listenerOrderMatters = listenerOrderMatters || listenerOrderMatterForThisEvent;
+        }
+        
+        if(listenerOrderMatters)
+            callInfo = callInfo.OrderBy(x => x.Priority ?? double.NegativeInfinity);
+
+        foreach(var c in callInfo)
+            c.CallListener();
+    }
     
+    /// <summary>
+    /// Invokes multiple events together at the same time, passed the same IEventArgs object to each one, which may be
+    /// used to generate the IEventArgs objects passed to any dependent events.
+    ///
+    /// Listeners from all events are called in a single pool respecting priority if applicable. e.g. if event A and
+    /// event B are invoked together with this static method, and event A has a listener with a higher priority than
+    /// event B's listener, and a listener with a lower priority than event B's listener, then event A's lower priority
+    /// listener will be called first, followed by event B's listener, followed by event A's higher priority listener.
+    /// </summary>
+    /// <param name="toInvoke">The events to invoke.</param>
+    /// <param name="args">The event args to pass to all directly invoked events.</param>
+    public static void InvokeMultiple<TArgs>(IEnumerable<IInvocableEvent<TArgs>> toInvoke, TArgs args)
+        where TArgs : IEventArgs
+    {
+        var callInfo             = Enumerable.Empty<IEventListenerCallInfo>();
+        var listenerOrderMatters = false;
+
+        foreach(var ev in toInvoke)
+        {
+            callInfo             = callInfo.Concat(ev.GenerateCallInfo(args, out var listenerOrderMatterForThisEvent));
+            listenerOrderMatters = listenerOrderMatters || listenerOrderMatterForThisEvent;
+        }
+        
+        if(listenerOrderMatters)
+            callInfo = callInfo.OrderBy(x => x.Priority ?? double.NegativeInfinity);
+
+        foreach(var c in callInfo)
+            c.CallListener();
+    }
 }
