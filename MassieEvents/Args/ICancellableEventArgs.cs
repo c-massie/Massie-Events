934c39d8a3a62acbbe9cdbbd25ef87a3956471cd
c-massie <77603178+c-massie@users.noreply.github.com>
2025-02-03 17:35:23 +0000

    Added an example to ICancellableEventArgs.
    Added an example to the inline documentation of ICancellableEventArgs.

--- MassieEvents/Args/ICancellableEventArgs.cs
+++ MassieEvents/Args/ICancellableEventArgs.cs
@@ -4,6 +4,18 @@ namespace Scot.Massie.Events.Args;
 /// Cancellable event. Generally, the event of an event args object implementing this would be fired before the actual
 /// thing happening, and would check the event args object to see if the thing should happen or not.
 /// </summary>
+/// <example>
+/// <code>
+/// var args = new SomeCancellableEventArgs();
+/// 
+/// this.MyEvent.Invoke(args);
+/// 
+/// if(!args.EventIsCancelled)
+/// {
+///     DoTheThing();
+/// }
+/// </code>
+/// </example>
 public interface ICancellableEventArgs : IEventArgs
 {
     /// <summary>
