13d91847032e7c2adb764c93dba2311c7ec45425
c-massie <77603178+c-massie@users.noreply.github.com>
2025-02-03 04:41:59 +0000

    Added ICancellableEventArgs, added inline documentation for event args.
    Added `ICancellableEventArgs`.
    
    To event args interfaces, added inline documentation.

---
+++ MassieEvents/Args/ICancellableEventArgs.cs
@@ -0,0 +1,14 @@
+namespace Scot.Massie.Events.Args;
+
+/// <summary>
+/// Cancellable event. Generally, the event of an event args object implementing this would be fired before the actual
+/// thing happening, and would check the event args object to see if the thing should happen or not.
+/// </summary>
+public interface ICancellableEventArgs : IEventArgs
+{
+    /// <summary>
+    /// Whether the event is cancelled. If false, the thing will happen as expected. If true, the thing shouldn't
+    /// happen.
+    /// </summary>
+    bool EventIsCancelled { get; }
+}
--- MassieEvents/Args/IEventArgs.cs
+++ MassieEvents/Args/IEventArgs.cs
@@ -1,5 +1,8 @@
 namespace Scot.Massie.Events.Args;
 
+/// <summary>
+/// Base interface for all event args objects used with the event system in the `Scot.Massie.Events` package.
+/// </summary>
 public interface IEventArgs
 {
     
