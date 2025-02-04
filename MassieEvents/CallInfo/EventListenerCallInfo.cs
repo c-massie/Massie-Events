5fd45681041f0dead1523392c45661b41ae8ac8d
c-massie <77603178+c-massie@users.noreply.github.com>
2025-02-04 09:00:36 +0000

    Added inline documentation to IEventListenerCallInfo & implementation
    Added inline documentation to IEventListenerCallInfo & implementation

--- MassieEvents/CallInfo/EventListenerCallInfo.cs
+++ MassieEvents/CallInfo/EventListenerCallInfo.cs
@@ -1,16 +1,25 @@
-using Scot.Massie.Events.Args;
-
 namespace Scot.Massie.Events.CallInfo;
 
+/// <inheritdoc cref="IEventListenerCallInfo{TArgs}"/>
 public class EventListenerCallInfo<TArgs> : IEventListenerCallInfo<TArgs>
     where TArgs : EventArgs
 {
+    /// <inheritdoc />
     public EventListener<TArgs> Listener { get; }
 
+    /// <inheritdoc />
     public double? Priority { get; }
 
+    /// <inheritdoc />
     public TArgs Args { get; }
 
+    /// <summary>
+    /// Creates a new call info object, pairing a listener with the args to be passed to it when it's called, also
+    /// including the event priority, if applicable.
+    /// </summary>
+    /// <param name="listener">The listener that may be called.</param>
+    /// <param name="priority">The priority of the listener upon event invocation.</param>
+    /// <param name="args">The event args object to be passed to the listener when it's called.</param>
     public EventListenerCallInfo(EventListener<TArgs> listener, double? priority, TArgs args)
     {
         Listener = listener;
@@ -18,6 +27,7 @@ public class EventListenerCallInfo<TArgs> : IEventListenerCallInfo<TArgs>
         Args     = args;
     }
 
+    /// <inheritdoc />
     public void CallListener()
     {
         Listener(Args);
--- MassieEvents/CallInfo/IEventListenerCallInfo.cs
+++ MassieEvents/CallInfo/IEventListenerCallInfo.cs
@@ -1,18 +1,37 @@
-using Scot.Massie.Events.Args;
-
 namespace Scot.Massie.Events.CallInfo;
 
+/// <summary>
+/// An <see cref="EventListener{TArgs}"/> paired with the information required to call it.
+/// </summary>
 public interface IEventListenerCallInfo
 {
+    // ReSharper disable once GrammarMistakeInComment
+    /// <summary>
+    /// The priority of the event listener. The higher, the later. Listeners with a priority of null will be called
+    /// first.
+    /// </summary>
     public double? Priority { get; }
 
+    /// <summary>
+    /// Calls the event listener, passing in the relevant <see cref="EventArgs"/> object.
+    /// </summary>
     public void CallListener();
 }
 
+/// <summary>
+/// An <see cref="EventListener{TArgs}"/> paired with the information required to call it.
+/// </summary>
+/// <typeparam name="TArgs">The type of the event args object.</typeparam>
 public interface IEventListenerCallInfo<TArgs> : IEventListenerCallInfo
     where TArgs : EventArgs
 {
+    /// <summary>
+    /// The listener that can be called.
+    /// </summary>
     public EventListener<TArgs> Listener { get; }
 
+    /// <summary>
+    /// The <see cref="EventArgs"/> object intended to be passed to the listener when the listener is called.
+    /// </summary>
     public TArgs Args { get; }
 }
